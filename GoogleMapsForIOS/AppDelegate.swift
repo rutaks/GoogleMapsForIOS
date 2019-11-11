25d0a7dddd5aeff8307eef53e01feb7c1501f324
rutaks <samrutakayile@awesomity.rw>
2019-11-11 05:57:05 +0200

    Setup Google API Keys

--- GoogleMapsForIOS/AppDelegate.swift
+++ GoogleMapsForIOS/AppDelegate.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Setup Of Google Maps Services By Setting App To Service On Launch
+        //Must Create A Project & YOUR API Key
+        GMSServices.provideAPIKey("YOUR_API_KEY")
+        //If You are using Google Places, use the below as well
+        GMSPlacesClient.provideAPIKey("YOUR_API_KEY")
         return true
     }
 
