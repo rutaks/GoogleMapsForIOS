e2373b66d90e418c41d01494d3a1f4d662855ace
rutaks <samrutakayile@awesomity.rw>
2019-11-11 06:12:37 +0200

    Setup Map View On Load First Screen

--- GoogleMapsForIOS/ViewController.swift
+++ GoogleMapsForIOS/ViewController.swift
@@ -15,16 +15,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        // Coordinate -1.962519, 30.106779 at zoom level 6.
+        // Coordinates Are Remera Kigali Rwanda
+        let camera = GMSCameraPosition.camera(withLatitude: -1.962519, longitude: 30.106779, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: -1.962519, longitude: 30.106779)
+        marker.title = "Kigali"
+        marker.snippet = "Rwanda"
         marker.map = mapView
     }
 
